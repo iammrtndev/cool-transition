3703e01692e9f5eb7721fcf381a92812537bbed7
iammrtndev <mr.martinpham@gmail.com>
2022-11-07 06:46:07 +0000

    css animation attempt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
 	import { navigating, page } from '$app/stores'
-	import { onMount } from 'svelte'
+
+	function waitForAnimationEnd(target: HTMLElement) {
+		return new Promise((resolve) => {
+			document.addEventListener('animationend', (event: AnimationEvent) => {
+				if (event.target == target) resolve(target)
+			})
+		})
+	}
+
+	function waitForTransitionEnd(target: HTMLElement) {
+		return new Promise((resolve) => {
+			document.addEventListener('transitionend', (event: TransitionEvent) => {
+				if (event.target == target) resolve(target)
+			})
+		})
+	}
 
 	type Page = {
 		route: string
 		text: string
 		color: string
-		aEl?: HTMLAnchorElement
-		bgEl?: HTMLDivElement
+		a?: HTMLAnchorElement
+		bg?: HTMLDivElement
 		txt?: HTMLDivElement
+		txtSpan?: HTMLSpanElement
 	}
 	const pages: Page[] = [
 		{ route: '/ressources', text: 'Ressources', color: '#ff667d' },
@@ -17,105 +33,93 @@
 	]
 	let routeId = $page.routeId!
 	let nextRouteId = routeId!
-	let animate = false
 
 	const widthVW = 9
 
 	navigating.subscribe(async (nav) => {
 		if (nav == null) return
 
+		pages.forEach((p) => {
+			p.txtSpan!.classList.add('hide')
+		})
+		await Promise.all(pages.map((p) => waitForTransitionEnd(p.txtSpan!)))
 		const fromIdx = pages.findIndex((p) => p.route == nav.from!.routeId)
 		const toIdx = pages.findIndex((p) => p.route == nav.to!.routeId)
-		const leftToRight = fromIdx > toIdx
+		const ltr = fromIdx > toIdx
 		nextRouteId = nav.to!.routeId!
 
 		const pagesSlide = pages.slice(Math.min(fromIdx, toIdx), Math.max(fromIdx, toIdx) + 1)
 		pagesSlide.forEach((p) => {
-			const route = p.aEl?.href.replace(location.origin, '')
-			const active = route == routeId
-			const activeNext = route == nextRouteId
-			console.log(route)
+			p.a?.classList.add(ltr ? 'ltr' : 'rtl')
+			// 	p.bg!.style.setProperty('--transform-origin-Start', 'left')
+			// 	p.bg!.style.setProperty('--transform-origin-End', 'right')
+			// 	p.bg!.style.setProperty('--transaleX-Start', '0vw')
+			// 	p.bg!.style.setProperty('--transaleX-End', `${100 - widthVW * (pages.length - (activeNext ? 1 : 0))}vw`)
+			// 	p.bg!.style.setProperty('--scale3d-x-Start', `${active ? 0 : 1}`)
+			// 	p.bg!.style.setProperty('--scale3d-x-Mid', `${(100 - widthVW * (pages.length - 1)) / widthVW}`)
+			// 	p.bg!.style.setProperty('--scale3d-x-End', `${activeNext ? 0 : 1}`)
+			// 	p.txt!.style.setProperty('--transaleX-Start', '0vw')
+			// 	p.txt!.style.setProperty('--transaleX-End', `${100 - widthVW * (pages.length - (activeNext ? 1 : 0))}vw`)
+			// }
+			// else {
+			// p.a!.style.setProperty('--delay', `${pages.indexOf(p) * 0.1}s`)
+			// 		p.bg!.style.setProperty('--transform-origin-Start', 'right')
+			// 		p.bg!.style.setProperty('--transform-origin-End', 'left')
+			// 		p.bg!.style.setProperty('--transaleX-Start', active ? `${100 - widthVW * pages.length}vw` : '0vw')
+			// 		p.bg!.style.setProperty('--transaleX-End', active ? '0' : `-${100 - widthVW * (pages.length - (activeNext ? 1 : 0))}vw`)
+			// 		p.bg!.style.setProperty('--scale3d-x-Start', `${active ? 0 : 1}`)
+			// 		p.bg!.style.setProperty('--scale3d-x-Mid', `${(100 - widthVW * (pages.length - 1)) / widthVW}`)
+			// 		p.bg!.style.setProperty('--scale3d-x-End', `${activeNext ? 0 : 1}`)
+			// 	}
+			// p.a!.classList.add('animate')
+		})
+		// const pagesStatic = pages.filter((p) => !pagesSlide.includes(p))
+		// pagesStatic.forEach((p) => {
+		// 	const translateX = '0vw'
+		// 	const scale3dX = '1'
+		// 	p.bg!.style.setProperty('--transaleX-Start', translateX)
+		// 	p.bg!.style.setProperty('--transaleX-End', translateX)
+		// 	p.bg!.style.setProperty('--scale3d-x-Start', scale3dX)
+		// 	p.bg!.style.setProperty('--scale3d-x-Mid', scale3dX)
+		// 	p.bg!.style.setProperty('--scale3d-x-End', scale3dX)
 
-			if (leftToRight) {
-				p.aEl!.style.setProperty('--delay', `${(pages.length - pages.indexOf(p) - 1) * 0.1}s`)
-				p.bgEl!.style.setProperty('--transform-origin-Start', 'left')
-				p.bgEl!.style.setProperty('--transform-origin-End', 'right')
-				p.bgEl!.style.setProperty('--transaleX-Start', '0vw')
-				p.bgEl!.style.setProperty('--transaleX-End', `${100 - widthVW * (pages.length - (activeNext ? 1 : 0))}vw`)
-				p.bgEl!.style.setProperty('--scale3d-x-Start', `${active ? 0 : 1}`)
-				p.bgEl!.style.setProperty('--scale3d-x-Mid', `${(100 - widthVW * (pages.length - 1)) / widthVW}`)
-				p.bgEl!.style.setProperty('--scale3d-x-End', `${activeNext ? 0 : 1}`)
-			} else {
-				p.aEl!.style.setProperty('--delay', `${pages.indexOf(p) * 0.1}s`)
-				p.bgEl!.style.setProperty('--transform-origin-Start', 'right')
-				p.bgEl!.style.setProperty('--transform-origin-End', 'left')
-				p.bgEl!.style.setProperty('--transaleX-Start', `${100 - widthVW * (pages.length - (active ? 1 : 0))}vw`)
-				p.bgEl!.style.setProperty('--transaleX-End', '0vw')
-				p.bgEl!.style.setProperty('--scale3d-x-Start', `${active ? 0 : 1}`)
-				p.bgEl!.style.setProperty('--scale3d-x-Mid', `${(100 - widthVW * (pages.length - 1)) / widthVW}`)
-				p.bgEl!.style.setProperty('--scale3d-x-End', `${active ? 0 : 1}`)
-			}
-		})
-		const pagesStatic = pages.filter((p) => !pagesSlide.includes(p))
-		pagesStatic.forEach((p) => {
-			if (leftToRight) {
-				const translateX = '0vw'
-				const scale3dX = '1'
-				p.bgEl!.style.setProperty('--transaleX-Start', translateX)
-				p.bgEl!.style.setProperty('--transaleX-End', translateX)
-				p.bgEl!.style.setProperty('--scale3d-x-Start', scale3dX)
-				p.bgEl!.style.setProperty('--scale3d-x-Mid', scale3dX)
-				p.bgEl!.style.setProperty('--scale3d-x-End', scale3dX)
-			} else {
-				const translateX = `${100 - widthVW * pages.length}vw`
-				const scale3dX = `${(100 - widthVW * (pages.length - 1)) / widthVW}`
-				p.bgEl!.style.setProperty('--transaleX-Start', translateX)
-				p.bgEl!.style.setProperty('--transaleX-End', translateX)
-				p.bgEl!.style.setProperty('--scale3d-x-Start', scale3dX)
-				p.bgEl!.style.setProperty('--scale3d-x-Mid', scale3dX)
-				p.bgEl!.style.setProperty('--scale3d-x-End', scale3dX)
-			}
-		})
-		animate = true
-	})
+		// 	p.txt!.style.setProperty('--transaleX-Start', '0vw')
+		// 	p.txt!.style.setProperty('--transaleX-End', '0')
+		// })
 
-	onMount(() => {
-		document.addEventListener('animationend', (e) => {
-			animate = false
-			// routeId = nextRouteId
+		await Promise.all(pagesSlide.map((p) => waitForAnimationEnd(p.a!)))
+
+		routeId = nextRouteId
+		nextRouteId = ''
+
+		pages.forEach((p) => {
+			p.txtSpan!.classList.remove('hide')
 		})
+		await Promise.all(pages.map((p) => waitForTransitionEnd(p.txtSpan!)))
 	})
 </script>
 
 <aside>
-	{#each pages as page, i}
+	{#each pages as page}
 		<a
-			bind:this={page.aEl}
-			class:animate
+			bind:this={page.a}
 			class:active={page.route == routeId}
+			class:next={page.route == nextRouteId}
 			href={page.route}
 			style={`
 				width: ${widthVW}vw;
 			`}
 		>
 			<div
-				bind:this={page.bgEl}
+				bind:this={page.bg}
 				class="bg"
 				style={`
 					background: ${page.color};
 					max-width: ${widthVW}vw;
 				`}
 			/>
-			<div
-				bind:this={page.txt}
-				class="txt"
-				style={`
-					visibility: ${page.route == routeId ? 'hidden' : 'visible'};
-					--transaleX-Start: 0vw;
-					--transaleX-End: ${100 - widthVW * pages.length}vw;
-				`}
-			>
-				<span>
+			<div bind:this={page.txt} class="txt">
+				<span bind:this={page.txtSpan} class="txtSpan">
 					{page.text}
 				</span>
 			</div>
@@ -145,26 +149,22 @@
 	a {
 		position: relative;
 		height: 100%;
-		width: var(--linkWidth);
 		text-decoration: none;
 	}
 	a.active {
 		pointer-events: none;
-		flex: 1;
 	}
-
-	a .bg {
-		transform: translateX(0) scale3d(1, 1, 1);
+	.bg {
 		position: absolute;
 		height: 100%;
 		width: 100%;
 	}
 	a.active .bg {
-		transform: translateX(0) scale3d(0, 1, 1);
+		transform: scale3d(0, 1, 1);
 	}
 
-	a .txt {
-		color: white;
+	.txt {
+		color: black;
 		font-family: Arial, Helvetica, sans-serif;
 		font-size: xx-large;
 		position: absolute;
@@ -175,76 +175,81 @@
 		overflow: hidden;
 	}
 
-	a .txt span {
-		position: relative;
+	a.active .txt {
+		color: transparent;
 	}
 
-	:global(a.animate .txt),
-	:global(a.animate .txt span),
-	:global(a.animate .bg) {
-		animation-duration: 3s;
-		animation-timing-function: cubic-bezier(0.645, 0.045, 0.355, 1); /* easeInOutCubic */
+	.txtSpan {
+		position: relative;
+		right: 0px;
+		transition: all 500ms cubic-bezier(0.55, 0.055, 0.675, 0.19); /* easeInCubic */
+	}
+	:global(.txtSpan.hide) {
+		right: 40px !important;
+	}
+
+	a,
+	.txt,
+	.bg {
+		animation-duration: 2s;
+		animation-timing-function: cubic-bezier(0.77, 0, 0.175, 1); /* easeInOutQuart */
 		animation-fill-mode: forwards;
 		animation-delay: var(--delay);
 	}
-	:global(a.animate .txt) {
-		animation-name: txt;
+	:global(a.ltr:nth-child(1)) {
+		--delay: 0.2s;
 	}
-	:global(a.animate .txt span) {
-		animation-name: txtSpan;
+	:global(a.ltr:nth-child(2)) {
+		--delay: 0.1s;
 	}
-	:global(a.animate .bg) {
-		animation-name: bg;
+	:global(a.ltr:nth-child(3)) {
+		--delay: 0s;
 	}
-	@keyframes txt {
+	:global(a.ltr) {
+		animation-name: ltrA;
+		--translateX-End: 73vw;
+	}
+	:global(a.next.ltr) {
+		animation-name: ltrA;
+		--translateX-End: 82vw;
+	}
+	@keyframes ltrA {
 		0% {
-			transform: translateX(var(--transaleX-Start));
+			transform: translateX(0);
 		}
-		15% {
-			transform: translateX(var(--transaleX-Start));
+		50% {
+			transform: translateX(0);
 		}
 
 		100% {
-			transform: translateX(var(--transaleX-End));
+			transform: translateX(var(--translateX-End));
 		}
 	}
-	@keyframes txtSpan {
-		0% {
-			right: 0px;
-		}
-		15% {
-			right: 40px;
-		}
 
-		90% {
-			right: 40px;
-		}
-		100% {
-			right: 0%;
-		}
+	:global(a.ltr .bg) {
+		transform-origin: left;
+		animation-name: ltrBg;
+		--scale3d-x-Start: 1;
+		--scale3d-x-Mid: calc(82 / 9);
+		--scale3d-x-End: 1;
 	}
-	@keyframes bg {
+	:global(a.active.ltr .bg) {
+		--scale3d-x-Start: 0;
+	}
+	:global(a.next.ltr .bg) {
+		--scale3d-x-End: 0;
+	}
+	@keyframes ltrBg {
 		0% {
-			transform-origin: var(--transform-origin-Start);
-			transform: translateX(0) scale3d(var(--scale3d-x-Start), 1, 1);
-		}
-		20% {
-			transform-origin: var(--transform-origin-Start);
-			transform: translateX(0) scale3d(var(--scale3d-x-Start), 1, 1);
+			transform: scale3d(var(--scale3d-x-Start), 1, 1);
 		}
 
 		50% {
-			transform-origin: var(--transform-origin-Start);
-			transform: translateX(0) scale3d(var(--scale3d-x-Mid), 1, 1);
+			transform: scale3d(var(--scale3d-x-Mid), 1, 1);
 		}
 
-		80% {
-			transform-origin: var(--transform-origin-Start);
-			transform: translateX(var(--transaleX-End)) scale3d(var(--scale3d-x-End), 1, 1);
-		}
 		100% {
-			transform-origin: var(--transform-origin-Start);
-			transform: translateX(var(--transaleX-End)) scale3d(var(--scale3d-x-End), 1, 1);
+			transform: scale3d(var(--scale3d-x-End), 1, 1);
 		}
 	}
 </style>
